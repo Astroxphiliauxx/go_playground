8b483f2962de6bf55bf01ba8212b15dc25840b6f
astroxphiliauxx <astroxphiliauxx@gmail.com>
2025-08-01 02:27:50 +0530

    pretty good handles

--- 04time_handling/main.go
+++ 04time_handling/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"time"
 )
 
@@ -12,5 +13,8 @@ func main() {
 
 	fmt.Println("Formatted time:", currTime.Format("01-02-2006 15:04:05 Monday"))
 	// well i m still surprised that why using this format !!!!
+
+	diceNumber := rand.Intn(6) + 1 // generates a random number between 1 and 6
+	fmt.Println("You rolled a dice and got:", diceNumber)
 
 }

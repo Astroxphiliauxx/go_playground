ea7a0e883edd104a2e2d2fb369081a983c7a1211
astroxphiliauxx <astroxphiliauxx@gmail.com>
2025-08-01 02:28:18 +0530

    done

--- 03conversions/main.go
+++ 03conversions/main.go
@@ -16,8 +16,9 @@ func main() {
 
 	fmt.Println("You entered:", input)
 
-	numRating, err := strconv.ParseInt(strings.TrimSpace(input), 10, 64)
+	numRating, err := strconv.ParseInt(strings.TrimSpace(input), 10, 64) //conversion of string into int64, trimming is necessary to avoid any errors.
 
+	//error handling
 	if err != nil {
 		fmt.Println("Error converting input to number:", err)
 		return
